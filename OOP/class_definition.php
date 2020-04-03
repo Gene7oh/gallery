f7a9882ab433e1f2aaf9f1905ecd2d6fcfd2e110
Gene7oh <gene7oh@hotmail.com>
2020-04-03 18:13:31 -0500

    Inital Commit

--- OOP/class_definition.php
+++ OOP/class_definition.php
@@ -1,2 +1,10 @@
 <?php
-
+    /* COMMENTED OUT SO PHPSTORM WON'T SCREAM AT ME
+    class Cars
+    {
+    
+    }
+    $classes = get_declared_classes();
+    foreach ($classes as $class){
+        echo $class . "<br>";
+    }*/
---
+++ OOP/class_methods.php
@@ -0,0 +1,24 @@
+<?php
+    
+    /**
+     * Created by gene7
+     * Using PhpStorm
+     * Date: 4/3/2020
+     */
+    class Cars
+    {
+        function greeting()
+        {
+        
+        }
+        
+        function salutation()
+        {
+        
+        }
+    }
+    
+    $class_methods = get_class_methods('Cars');
+    foreach ($class_methods as $method) {
+        echo $method . "<br>";
+    }
