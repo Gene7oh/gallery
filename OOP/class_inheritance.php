7dcea7e93077bc182a864b48ba58993fcb6ea84c
Gene7oh <gene7oh@hotmail.com>
2020-04-08 21:02:56 -0500

    Push 4th part to the hub

--- OOP/class_inheritance.php
+++ OOP/class_inheritance.php
@@ -1,5 +1,5 @@
 <?php
-    /*
+    
     class Cars
     {
         var $wheel_count = 4;
@@ -11,9 +11,4 @@
         
     }
     $bmw = new Cars();
-    echo $bmw->wheel_count . "<br>";
-    echo $bmw->door_count . "<br>";
-    $volvo = new Cars();
-    $volvo->door_count = 4;
-    echo $volvo->car_details();
-    echo $bmw->car_details();*/
+    echo $bmw->car_details();
