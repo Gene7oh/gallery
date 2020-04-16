d32c5a06858349d195653a8e8045252f6f2573a1
Gene7oh <gene7oh@hotmail.com>
2020-04-15 23:28:45 -0500

    Add paragraph format to index

--- index.php
+++ index.php
@@ -1,30 +1,15 @@
 <?php include("includes/header.php"); ?>
-
-
-        <div class="row">
-
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
-
-    
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
-        </div>
-        <!-- /.row -->
-
-        <?php include("includes/footer.php"); ?>
+<div class="row">
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
+        <h2>Gallery Headline</h2>
+        <p class="text-justify lead">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consectetur delectus deserunt, maxime, numquam obcaecati pariatur perferendis quia quo repellat sequi soluta ut velit! Illo inventore itaque neque perspiciatis quis.
+        </p>
+    </div>
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4">
+        <?php include("includes/sidebar.php"); ?>
+    </div>
+    <!-- /.row -->
+    <?php include("includes/footer.php"); ?>
