2d67d55fefd5bd6c4a22932016004a84724bc2ab
Gene7oh <gene7oh@hotmail.com>
2020-04-18 14:35:53 -0500

    Refactor admin index navigation to the includes folder. Add admin content page to index folder refactor admin/index

--- admin/index.php
+++ admin/index.php
@@ -1,37 +1,18 @@
 <?php include("includes/admin_header.php"); ?>
-
-        <!-- Navigation -->
-<?php
-    include "includes/admin_nav.php";
-?>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <?php include "includes/admin_topnav.php"; ?>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <?php include "includes/admin_sidenav.php" ?>
+        <!-- /.navbar-collapse -->
+    </nav>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <?php include "includes/admin_content.php"; ?>
         </div>
-        <!-- /#page-wrapper -->
-
-  <?php include("includes/admin_footer.php"); ?>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+<?php include("includes/admin_footer.php"); ?>
