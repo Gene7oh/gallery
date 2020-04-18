1d9b98238e0044b6fd8e16fc00eec9275cb20b6d
Gene7oh <gene7oh@hotmail.com>
2020-04-18 14:39:21 -0500

    Add lorem to admin index for text place hold

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,6 +4,7 @@
             Blank Page
             <small>Subheading</small>
         </h1>
+        <p class="lead text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate deleniti dicta dolorem eaque nemo nobis perferendis suscipit tempore velit. Ab aliquam hic libero natus nihil quisquam rem ullam veniam?</p>
         <ol class="breadcrumb">
             <li>
                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
