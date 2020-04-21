99e2764bf8917cc34ee8e9d946ca9636811e4a0f
Gene7oh <gene7oh@hotmail.com>
2020-04-21 17:32:19 -0500

    create db set connection create a database class. nearly ready to merge branch

---
+++ admin/includes/database.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "config.php";
+    
+    class Database
+    {
+        public $connect; /** original diaz var was private phpstorm thru a fit and edwin from the future makes it public which is one of the options phpstorm offered Cool right */
+        
+        public function connect_db()
+        {
+            $this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+            if (mysqli_connect_errno()) {
+                echo "Database Connection Failed " . mysqli_error();
+            }
+        }
+    }
+    
+    $database = new Database();
+    $database->connect_db();
+    
