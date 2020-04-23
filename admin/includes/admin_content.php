9d954a59cc344c46af31f0f586f3522869999724
Gene7oh <gene7oh@hotmail.com>
2020-04-22 22:53:13 -0500

    Set database connection with construct

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -4,7 +4,13 @@
             Admin Page
             <small>administration @gallery</small>
         </h1>
+        <h2>Admin Content Page <small>actual</small> </h2>
         <p class="lead text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate deleniti dicta dolorem eaque nemo nobis perferendis suscipit tempore velit. Ab aliquam hic libero natus nihil quisquam rem ullam veniam?</p>
+        <?php
+        if ($database->connect){
+            echo "Connected";
+        }
+        ?>
         <ol class="breadcrumb">
             <li>
                 <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
--- admin/includes/database.php
+++ admin/includes/database.php
@@ -3,8 +3,12 @@
     
     class Database
     {
-        public string $connect; /** original diaz var was private phpstorm thru a fit and edwin from the future makes it public which is one of the options phpstorm offered Cool right */
-        
+        public $connect; /** original diaz var was private phpstorm thru a fit and edwin from the future makes it public which is one of the options phpstorm offered Cool right */
+        function __construct()
+        {
+            $this->connect_db();
+        }
+    
         public function connect_db()
         {
             $this->connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
@@ -15,5 +19,3 @@
     }
     
     $database = new Database();
-    $database->connect_db();
-    
