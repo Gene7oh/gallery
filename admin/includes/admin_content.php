5c098211deb0bdfa3c0ce84542628dfe10d09400
Gene7oh <gene7oh@hotmail.com>
2020-05-08 12:09:15 -0500

    Create objects from User class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,18 +7,21 @@
         <h2>Admin Content Page <small>actual</small></h2>
         <p class="lead text-justify">
             <?php
-                $result_set = Users::find_all_users();
+                /*$result_set = Users::find_all_users();
                 while ($row = mysqli_fetch_array($result_set)) {
                     echo $row['user_fname'] . "<br>";
                 }
                 $user_id = 3;
                 $found_user = Users::user_by_id($user_id);
                 if (!$found_user){
-                    echo "Unable to locate User " . "<br>";
+                    echo "Unable to locate User With User IF of " . $user_id . "<br>";
                 } else {
-                    echo $found_user['user_name'] . " has been located " . "<br>";
-                }
-                
+                    echo $found_user['user_name'] . " has been located. The user ID is " . $found_user['user_id'] . "<br>";
+                }*/
+                $id = 2;
+                $find_user = Users::user_by_id($id);
+                $user_found = Users::instantiate($find_user);
+                echo "The user named " . $user_found->fname . " has been found listed under the Id of " . $user_found->id . "<br>";
             ?>
         </p>
         <ol class="breadcrumb">
--- admin/includes/user_class.php
+++ admin/includes/user_class.php
@@ -2,16 +2,37 @@
     
     class Users
     {
+        public int    $id;
+        public string $user_name;
+        public string $password;
+        public string $fname;
+        public string $lname;
+        
         public static function find_all_users()
         {
-            global $database;
-            return $result_set = $database->query("SELECT * FROM users");
+            return self::find_this_query("SELECT * FROM users");
         }
         
         public static function user_by_id($user_id)
         {
-            global $database;
-            $result_set = $database->query("SELECT * FROM users WHERE user_id =$user_id");
+            $result_set = self::find_this_query("SELECT * FROM users WHERE user_id =$user_id");
             return $found_user = mysqli_fetch_array($result_set);
+        }
+        
+        public static function find_this_query($sql)
+        {
+            global $database;
+            return $result_set = $database->query($sql);
+        }
+        
+        public static function instantiate($record)
+        {
+            $the_object            = new self();
+            $the_object->id        = $record['user_id'];
+            $the_object->user_name = $record['user_name'];
+            $the_object->password  = $record['user_password'];
+            $the_object->fname     = $record['user_fname'];
+            $the_object->lname     = $record['user_lname'];
+            return $the_object;
         }
     }
