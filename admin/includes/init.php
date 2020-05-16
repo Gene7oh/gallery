5d194fea61d8e1d246d6ab2da41c5951c3507b0a
Gene7oh <gene7oh@hotmail.com>
2020-05-16 14:38:45 -0500

    Change include() to require_once() in init.php for added security

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,9 +1,9 @@
 <?php
     /** may have to rename the config file in the future to new_config.php EDWIN FROM THE FUTURE */
-    include "Sessions.php";
-    include "admin_functions.php";
-    include "config.php";
-    include "Database.php";
-    include "Users.php";
+    require_once "admin_functions.php";
+    require_once "config.php";
+    require_once "Database.php";
+    require_once "Users.php";
+    require_once "Sessions.php";
     /*Cars class is for testing the auto load file function only
     include "Cars.php";*/
