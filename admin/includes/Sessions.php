c6d61b7407bed33e68e705dbb7c878d3b5dfced1
Gene7oh <gene7oh@hotmail.com>
2020-05-21 08:31:37 -0500

    Create logout method in Sessions Class. End Sessions Class coding

--- admin/includes/Sessions.php
+++ admin/includes/Sessions.php
@@ -33,14 +33,21 @@
         {
             return $this->signed_in;
         }
-    
+        
         public function login($user)
         {
             if ($user) {
-                $this->user_id = $_SESSION['user_id'] = $user->user_id;
+                $this->user_id   = $_SESSION['user_id'] = $user->user_id;
                 $this->signed_in = true;
             }
-    
+            
+        }
+        
+        public function logout()
+        {
+            unset($this->user_id);
+            unset($_SESSION['user_id']);
+            $this->signed_in = false;
         }
         
         private function check_the_login()
