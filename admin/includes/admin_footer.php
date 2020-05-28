78e187b08a6e6906fcd14654854d6fcc73b7c424
Gene7oh <gene7oh@hotmail.com>
2020-05-28 12:09:55 -0500

    Start the style for footer

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,14 +1,61 @@
 </div>
-<div class="lead" style="color: whitesmoke">
+<!--<div class="lead nav navbar-nav" style="color: whitesmoke">
     <h2 class="lead">Footer Stuff</h2>
-    <ul class="nav navbar-right top-right">
-        <li class="nav nav-item pull-left">1 item</li>
-        <li class="nav nav-item pull-left">2 item</li>
-        <li class="nav nav-item pull-left">3 item</li>
-        <li class="nav nav-item pull-left">4 item</li>
-        <li class="nav nav-item pull-left">5 item</li>
-    </ul>
-</div>
+        <li class=""><a href="#" class="navbar-brand">1 item</></li>
+        <li class=""><a href="#" class="navbar-brand">2 item</></li>
+        <li class=""><a href="#" class="navbar-brand">3 item</></li>
+        <li class=""><a href="#" class="navbar-brand">4 item</></li>
+        <li class=""><a href="#" class="navbar-brand">5 item</></li>
+</div>-->
+    <style>
+        /* todo create working hover class*/
+        .hover :hover {
+            color: darkred;
+            font-style: italic;
+            }
+    </style>
+<main style="color: whitesmoke" role="main" class="container">
+    <h1 class="mt">@Gallery Footer</h1>
+    <p class="lead">Various informative statements explaining the following links</p>
+    <div class="row">
+        <div class="col-md-3 navbar-brand">
+            <h3 class="media">Links</h3>
+            <ul class="">
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 navbar-brand">
+            <h3 class="media">Links</h3>
+            <ul class="">
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 navbar-brand">
+            <h3 class="media">Links</h3>
+            <ul class="">
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
+            </ul>
+        </div>
+        <div class="col-md-3 navbar-brand">
+            <h3 class="media">Links</h3>
+            <ul class="">
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
+                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
+            </ul>
+        </div>
+    </div>
+</main>
 <!-- /#wrapper -->
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
