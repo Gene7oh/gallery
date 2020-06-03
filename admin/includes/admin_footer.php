ae78065a525fcfc4fbcdf950e0a763571751c7a5
Gene7oh <gene7oh@hotmail.com>
2020-06-03 16:24:45 -0500

    Create admin footer link styles

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,6 +1,28 @@
 </div>
-<div class="lead nav navbar-nav" style="color: whitesmoke">
+<div class="container-fluid" style="color: whitesmoke">
     <h2 class="lead">Footer Stuff</h2>
+    <div class="row">
+        <div class="col-sm-3">
+            <h4>Column Header</h4>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus assumenda consequatur cum, cumque cupiditate delectus deleniti dolore enim eum id libero magni nobis nostrum quae sapiente sequi suscipit tempora.</p>
+            <a style="text-decoration: none; color: whitesmoke;" href="">link 1</a>
+        </div>
+        <div class="col-sm-3">
+            <h4>Column Header</h4>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus assumenda consequatur cum, cumque cupiditate delectus deleniti dolore enim eum id libero magni nobis nostrum quae sapiente sequi suscipit tempora.</p>
+            <a style="text-decoration: none; color: whitesmoke;" href="">link 2</a>
+        </div>
+        <div class="col-sm-3">
+            <h4>Column Header</h4>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus assumenda consequatur cum, cumque cupiditate delectus deleniti dolore enim eum id libero magni nobis nostrum quae sapiente sequi suscipit tempora.</p>
+            <a style="text-decoration: none; color: whitesmoke;" href="">link 3</a>
+        </div>
+        <div class="col-sm-3">
+            <h4>Column Header</h4>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus assumenda consequatur cum, cumque cupiditate delectus deleniti dolore enim eum id libero magni nobis nostrum quae sapiente sequi suscipit tempora.</p>
+            <a style="text-decoration: none; color: whitesmoke;" href="">link 4</a>
+        </div>
+    </div>
 </div>
 <!-- /#wrapper -->
 <!-- jQuery -->
