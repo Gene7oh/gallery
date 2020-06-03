325a344bbf71af2e93c0146c00da17d1df076708
Gene7oh <gene7oh@hotmail.com>
2020-06-03 13:02:53 -0500

    Move footer code from admin footer to root index footer. Remove style tag from all headers and pages. Ready to create style sheet for the footer

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,61 +1,7 @@
 </div>
-<!--<div class="lead nav navbar-nav" style="color: whitesmoke">
+<div class="lead nav navbar-nav" style="color: whitesmoke">
     <h2 class="lead">Footer Stuff</h2>
-        <li class=""><a href="#" class="navbar-brand">1 item</></li>
-        <li class=""><a href="#" class="navbar-brand">2 item</></li>
-        <li class=""><a href="#" class="navbar-brand">3 item</></li>
-        <li class=""><a href="#" class="navbar-brand">4 item</></li>
-        <li class=""><a href="#" class="navbar-brand">5 item</></li>
-</div>-->
-    <style>
-        /* todo create working hover class*/
-        .hover :hover {
-            color: darkred;
-            font-style: italic;
-            }
-    </style>
-<main style="color: whitesmoke" role="main" class="container">
-    <h3 class="mt">@Gallery Footer</h3>
-    <p class="lead">Various informative statements explaining the following links</p>
-    <div class="row">
-        <div class="col-md-3 navbar-brand">
-            <h3 class="media">Links</h3>
-            <ul class="">
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
-            </ul>
-        </div>
-        <div class="col-md-3 navbar-brand">
-            <h3 class="media">Links</h3>
-            <ul class="">
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
-            </ul>
-        </div>
-        <div class="col-md-3 navbar-brand">
-            <h3 class="media">Links</h3>
-            <ul class="">
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
-            </ul>
-        </div>
-        <div class="col-md-3 navbar-brand">
-            <h3 class="media">Links</h3>
-            <ul class="">
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">1 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">2 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">3 item</a></li>
-                <li class="nav"><a style="text-decoration: none;color: whitesmoke" href="" class="">4 item</a></li>
-            </ul>
-        </div>
-    </div>
-</main>
+</div>
 <!-- /#wrapper -->
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
--- includes/footer.php
+++ includes/footer.php
@@ -2,15 +2,30 @@
         <hr>
 
         <!-- Footer -->
-        <footer>
+        <main style="color: " role="main" class="container">
+            <h3 class="mt text-center">@Gallery Footer</h3>
+            <p class="lead text-center">Detailed listing of all information and location of the app's services and purpose. Detailed paragraphs with the links provided create navigation to the most important parts of the app.</p>
             <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; My Gallery 2014</p>
+                <!-- todo finish styles for links, finalize and copy for 3 columns -->
+                <div class="col-md-3 navbar-link">
+                    <h4 class="media text-center">Links</h4>
+                    <ul class="">
+                        <p class="lead">Various informative statements explaining the following links
+                        <li class="nav"><a style="text-decoration: none;color: "  href="" class="">1 item</a></li></p>
+                        <p class="lead">Various informative statements explaining the following links</p>
+                        <p class="lead">Various informative statements explaining the following links
+                        <li class="nav"><a style="text-decoration: none;color: " href="" class="">2 item</a></li></p>
+                        <p class="lead">Various informative statements explaining the following links</p>
+                        <p class="lead">Various informative statements explaining the following links
+                        <li class="nav"><a style="text-decoration: none;color: " href="" class="">3 item</a></li></p>
+                        <p class="lead">Various informative statements explaining the following links</p>
+                        <p class="lead">Various informative statements explaining the following links
+                        <li class="nav"><a style="text-decoration: none;color: " href="" class="">4 item</a></li></p>
+                        <p class="lead">Various informative statements explaining the following links</p>
+                    </ul>
                 </div>
-                <!-- /.col-lg-12 -->
             </div>
-            <!-- /.row -->
-        </footer>
+        </main>
 
     </div>
     <!-- /.container -->
