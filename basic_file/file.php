bd8ccbccadd07554b5f448742c5d44416cc781b6
Gene7oh <gene7oh@hotmail.com>
2020-06-05 15:41:55 -0500

    Learn basic file and directory manipulation

---
+++ basic_file/file.php
@@ -0,0 +1,16 @@
+<?php
+
+echo __FILE__ . "<br>";
+echo __LINE__ . "<br>";
+echo __DIR__ . "<br>";
+if (file_exists(__DIR__)){
+    echo "Affirmative Result" . "<br>";
+} else echo "No Results" . "<br>";
+if (is_file(__DIR__)){
+    echo "Affirmative! Path leads to a file." . "<br>";
+} else echo "False! Path does not lead to a file" . "<br>";
+if (is_dir(__DIR__)){
+    echo "Affirmative! Path leads to a folder." . "<br>";
+} else echo "False! Path does not lead to a folder" . "<br>";
+echo file_exists(__DIR__) ? "Located" . "<br>" : "Not Located" . "<br>";
+echo is_dir(__FILE__) ? "Yes a directory" . "<br>" : "No not a directory" . "<br>";
