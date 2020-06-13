a1ea233f829430ff39fb2250bfa815b97ea669c0
Gene7oh <gene7oh@hotmail.com>
2020-06-12 19:02:07 -0500

    Use CONSTANTS of the files super-global to display error or success messages.

--- basic_file/upload.php
+++ basic_file/upload.php
@@ -1,15 +1,3 @@
-<?php
-    /**
-     * Created by gene7
-     * Using PhpStorm
-     * Date: 6/11/2020
-     */
-    if (isset($_POST['submit'])) {
-        echo "<pre>";
-        print_r($_FILES['upload_file']);
-        echo "</pre>";
-    }
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -20,8 +8,38 @@
 </head>
 <body>
 <form action="" method="post" enctype="multipart/form-data">
+    <hr>
+    <h3>
+        <?php
+            /**
+             * Created by gene7
+             * Using PhpStorm
+             * Date: 6/11/2020
+             */
+            if (isset($_POST['submit'])) {
+                    echo "<pre>";
+                    print_r($_FILES['upload_file']);
+                    echo "</pre>";
+                    $upload_errors = array(
+                        UPLOAD_ERR_OK => 'The file successfully uploaded',
+                        UPLOAD_ERR_INI_SIZE => 'File exceeded max file size',
+                        UPLOAD_ERR_PARTIAL => 'Not all of the file was uploaded',
+                        UPLOAD_ERR_NO_FILE => 'You must choose a file first',
+                        UPLOAD_ERR_NO_TMP_DIR => 'Missing temp directory',
+                        UPLOAD_ERR_CANT_WRITE => 'Failed to write to disk',
+                        UPLOAD_ERR_EXTENSION => 'Incorrect file extension terminated the process'
+                    );
+                    $err = $_FILES['upload_file']['error'];
+                    $err_msg = $upload_errors[$err];
+                    if (!empty($upload_errors)){
+                        echo $err_msg;
+                    }
+            }
+        ?>
+    </h3>
     <input type="file" name="upload_file"> <br>
     <button type="submit" name="submit">Submit</button>
+    <hr>
 </form>
 <div>
     <button><a href="index.php">Home</a></button>
