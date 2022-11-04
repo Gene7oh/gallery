5e438e728061da2b68536e834c631345d6bf5b6a
Gene7oh <gene7oh@hotmail.com>
2022-11-04 15:09:33 -0500

    Afternoon commit: Create static method to find users by ID

--- admin/includes/admin-content.php
+++ admin/includes/admin-content.php
@@ -1,3 +1,7 @@
+<?php
+    /* php tag at the top to suppress errors due to php version differences */
+    /** @noinspection PhpUndefinedVariableInspection */
+?>
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="row">
@@ -7,22 +11,39 @@
                 <small>Admin Page</small>
             </h1>
             <?php
-                $sql        = "SELECT * FROM users WHERE user_id = 1";
-                echo "<pre>";
-                /** @noinspection PhpUndefinedVariableInspection */
-                $result     = $database->query($sql);
-                $user_found = mysqli_fetch_array($result);
-                echo "Found a user(s) named:" . "<br>" . $user_found['user_fname'] . "<br>" . $user_found['user_lname'];
-                echo "</pre>";
-                echo "<pre>";
-                $user = new User();
+                /*echo "<pre>";
+                 $sql    = "SELECT * FROM users";
+                 $result = $database->query($sql);
+                 while ($user_found = mysqli_fetch_array($result)) {
+                     echo "<pre>";
+                     echo "Found a user named:" . "<br>" . $user_found['user_fname'] . "<br>" . $user_found['user_lname'];
+                     echo "</pre>";
+                 }*/
+                // ↓↓ instantiate the class to access User methods↓↓
+                /*$user   = new User();
                 $result = $user->findAllUsers();
-                while ($row = mysqli_fetch_array($result)){
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<pre>";
+                    echo $row['user_id'] . " ";
                     echo $row['username'] . "<br>";
+                    echo "</pre>";
+                }*/
+                // ↓↓ Call same method using static method change method to static in User.php↓↓
+                /*$result = User::findAllUsers();
+                while ($row = mysqli_fetch_array($result)) {
+                    echo "<pre>";
+                    echo $row['user_id'] . " ";
+                    echo $row['username'];
+                    echo "</pre>";
                 }
+                echo "</pre>";*/
+                // ↓↓ Call user by ID using static method and passed parameter↓↓
+                $user_id = 1;
                 echo "<pre>";
+                $user_found = User::findUserById($user_id);
+                echo $user_found['user_id'] . " " . $user_found['username'];
+                echo "</pre>";
             ?>
-            
             <ol class="breadcrumb">
                 <li>
                     <i class="fa fa-dashboard"></i> <!--suppress HtmlUnknownTarget -->
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,10 +1,19 @@
 <?php
     
+    /** @noinspection PhpUnnecessaryLocalVariableInspection */
     class User {
-        public function findAllUsers() {
+        public static function findAllUsers() {
+            // ↓↓ to use the db class we need to make the $database object global↓↓
             global $database;
-            /** @noinspection PhpUnnecessaryLocalVariableInspection */
             $result_set = $database->query("SELECT * FROM users");
             return $result_set;
+        }
+    
+        /** @noinspection PhpUndefinedVariableInspection */
+        public static function findUserById($user_id){
+            global $database;
+            $result     = $database->query("SELECT * FROM users WHERE user_id = $user_id");
+            $found_user = mysqli_fetch_array($result);
+            return $found_user;
         }
     } /** end User class */
