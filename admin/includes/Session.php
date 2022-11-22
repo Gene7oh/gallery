69384362f80f4c1c2d245a0a9c39d2263c65ec2d
Gene7oh <gene7oh@hotmail.com>
2022-11-21 18:30:17 -0600

    Start Session Class

---
+++ admin/includes/Session.php
@@ -0,0 +1,47 @@
+<?php
+    
+    class Session
+    {
+        public int   $user_id;
+        private bool $signed_in = false;
+        
+        function __construct()
+        {
+            session_start();
+            $this->checkLogin();
+        }
+        
+        private function checkLogin()
+        {
+            if (isset($_SESSION['user_id'])) {
+                $this->user_id   = $_SESSION['user_id'];
+                $this->signed_in = true;
+            } else {
+                unset($this->user_id);
+                $this->signed_in = false;
+            }
+        }
+        
+        public function isSignedIn()
+        {
+            // method fetches private var is a getter method
+            return $this->signed_in;
+        }
+        
+        public function login(/*query the db*/ $user)
+        {
+            if ($user) {
+                /** @noinspection PhpUndefinedVariableInspection */
+                $this->user_id   = $_SESSION['user_id'] = $user->user_id;
+                $this->signed_in = true;
+            }
+        }
+        
+        public function logout()
+        {
+            unset($this->user_id);
+            unset($_SESSION['user_id']);
+            $this->signed_in = false;
+        }
+    } /* end the session class */
+    $session = new Session();
--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -10,13 +10,15 @@
             die("File $class not found! It was either deleted, renamed, or moved");
         }
     }*/
-function classAutoLoader($class){
+    function classAutoLoader($class)
+    {
 //    $class = strtolower($class);
-    $the_path = "includes/{$class}.php";
-    if (file_exists($the_path)){
-        require_once ($the_path);
-    } else {
-        die("<warning style='color:darkred'>The $class class was not found! <br> The file could have been deleted:<br> moved: renamed: or never created!</warning>");
+        $the_path = "includes/{$class}.php";
+        if (file_exists($the_path)) {
+            require_once($the_path);
+        } else {
+            die("<warning style='color:darkred'>The $class class was not found! <br> The file could have been deleted:<br> moved: renamed: or never created!</warning>");
+        }
     }
-}
-spl_autoload_register('classAutoLoader');
+    
+    spl_autoload_register('classAutoLoader');
