1680114b9f0ded6539dab83628af8e3965057a5d
Gene7oh <gene7oh@hotmail.com>
2022-12-23 19:16:23 -0600

    Define the system separator, the site root, and the includes folder to CONSTANTS. Defined in the init.php file but may move to the new-config.php file later on

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,4 +1,13 @@
 <?php
+    /** @noinspection PhpDefineCanBeReplacedWithConstInspection */
+    /* if (defined('DS')){
+         null;
+     } else {
+         define('DS', DIRECTORY_SEPARATOR);
+     }*/
+    defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+    define('SITE_ROOT', DS . 'Z:' . DS . 'XAMPP' . DS . 'HTDOCS' . DS . 'PROJECTS' . DS . 'GALLERY');
+    defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', 'SITE_ROOT' . DS . 'ADMIN' . DS . 'INCLUDES');
     require_once "functions.php";
     require_once "new-config.php";
     require_once "Database.php";
