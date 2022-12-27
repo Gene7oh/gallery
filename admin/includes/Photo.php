0dc4ee4d719d5cf90dc868a8d18d1e5100f7e6a9
Gene7oh <gene7oh@hotmail.com>
2022-12-27 14:56:47 -0600

    Create methods setFile and save 'which seems to over ride the save method in the parent class'. Create image folder. Start fine tuning methods for move_uploaded_file

--- admin/includes/Photo.php
+++ admin/includes/Photo.php
@@ -12,7 +12,7 @@
         public int              $size                = 0;
         public string           $tmp_path;
         public string           $upload_directory    = "images";
-        public array            $custom_errors_array = array();
+        public array            $errors              = array();
         public array            $upload_errors_array = array(
                 UPLOAD_ERR_OK         => "File successfully uploaded",
                 UPLOAD_ERR_INI_SIZE   => "File exceeds max upload file size",
@@ -23,5 +23,52 @@
                 UPLOAD_ERR_CANT_WRITE => "Failed to write to disk",
                 UPLOAD_ERR_EXTENSION  => "A PHP extension stopped the file upload"
         );
+        
+        // ↓↓ passes the FILES super global ['uploaded_file'] as an argument
+        public function setFile($file)
+        {
+            /** @noinspection PhpConditionAlreadyCheckedInspection */
+            if (empty($file) || !$file || !is_array($file)) {
+                $this->errors[] = "No file Uploaded";
+                return false;
+            } elseif ($file['error'] !== 0) {
+                $this->errors[] = $this->upload_errors_array[$file['error']];
+            } else {
+                $this->filename = basename($file['name']);
+                $this->tmp_path = $file['tmp_name'];
+                $this->type     = $file['type'];
+                $this->size     = $file['size'];
+            }
+            
+        }  /* End Method */
+        
+        public function save()
+        {
+            if ($this->id) {
+                $this->update();
+            } else {
+                if (!empty($this->errors)) {
+                    return false;
+                }
+                if (empty($this->filename) || empty($this->tmp_path)) {
+                    $this->errors[] = "File not available";
+                    return false;
+                }
+                $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+                if (file_exists($target_path)){
+                    $this->errors[] = "File {$this->filename} already exists";
+                    return false;
+                }
+                if (move_uploaded_file($this->tmp_path, $target_path)){
+                    if ($this->create()){
+                        unset($this->tmp_path);
+                        return true;
+                    }
+                } else {
+                    $this->errors[] = "Upload directory may need permissions";
+                    return false;
+                }
+            }
+        }  /* End Method */
         
     }  /* end of class */
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,10 +1,5 @@
 <?php
     /** @noinspection PhpDefineCanBeReplacedWithConstInspection */
-    /* if (defined('DS')){
-         null;
-     } else {
-         define('DS', DIRECTORY_SEPARATOR);
-     }*/
     defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
     define('SITE_ROOT', 'z:' . DS . 'xampp' . DS . 'htdocs' . DS . 'projects' . DS . 'gallery');
     defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
