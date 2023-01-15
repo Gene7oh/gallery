d987d5cb16ae248d9392cf8ca836209d6493bdaf
Gene7oh <gene7oh@hotmail.com>
2023-01-14 21:01:13 -0600

    Add method to display either user photo or image place holder to User class to call from the user page table. Fix css size issues. Create delete-user and edit-user pages for the next step

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -5,15 +5,16 @@
     
     class User extends Db_object
     {
-        protected static string $db_table        = "users";
-        protected static array  $db_table_fields = array('username', 'user_fname', 'user_lname', 'user_password');
-        public int              $id              = 0;
-        public string           $username        = "";
-        public string           $user_fname      = "";
-        public string           $user_lname      = "";
-        public string           $user_password   = "";
-        
-        /* @noinspection PhpMissingReturnTypeInspection */
+        protected static string $db_table          = "users";
+        protected static array  $db_table_fields   = array('username', 'user_fname', 'user_lname', 'user_image', 'user_password');
+        public int              $id                = 0;
+        public string           $username          = "";
+        public string           $user_fname        = "";
+        public string           $user_lname        = "";
+        public string           $user_image        = "";
+        public string           $user_password     = "";
+        public string           $upload_directory  = "images";
+        public string           $image_placeholder = "https://via.placeholder.com/250x200&text=user-profile-picture";
         
         public static function verifyUser($username, $password)
         {
@@ -24,6 +25,11 @@
             $sql          = "SELECT * FROM " . static::$db_table . " WHERE username = '{$username}' AND user_password = '{$password}' LIMIT 1";
             $result_array = User::findByQuery($sql);
             return !empty($result_array) ? array_shift($result_array) : false;
+        }
+        
+        public function placeholderOrImage()
+        {
+            return empty($this->user_image) ? $this->image_placeholder : $this->upload_directory . DS . $this->user_image;
         }
         
         
--- admin/users.php
+++ admin/users.php
@@ -1,11 +1,17 @@
-<?php include("includes/admin-header.php"); ?>
-    <title>Users Page</title>
+<?php /** @noinspection DuplicatedCode */
+    include("includes/admin-header.php"); ?>
+<?php
+    /** @noinspection PhpUndefinedVariableInspection */
+    if (!$session->isSignedIn()) {
+        redirect("login.php");
+    } ?>
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <!-- Brand and toggle get grouped for better mobile display -->
         <?php include "includes/navbar-header.php"; ?>
         <!-- Top Menu Items -->
-        <?php include('includes/admin-top-nav.php'); ?>
+        <?php
+            include('includes/admin-top-nav.php'); ?>
         <!--/* end top nav */-->
         <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
         <div class="collapse navbar-collapse navbar-ex1-collapse">
@@ -21,17 +27,49 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h1 class="page-header">
-                        User Page
-                        <small>Subheading</small>
+                        Gallery user
+                        <small>Display Page </small>
                     </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
+                    <?php
+                        /* if (isset($_GET['delete-success'])) {
+                             echo "<info style='color: darkblue'>user Successfully Deleted</info>";
+                             refresh(3, "users.php");
+                         }
+                         if (isset($_GET['no-user'])) {
+                             echo "<warning style='color: darkred'>Something went Wrong!</warning>";
+                         }*/
+                    ?>
+                    <div class="col-md-12 ">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Photo</th>
+                                <th>UserName</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                $users = User::findAll();
+                                foreach ($users as $user) : ?>
+                                    <tr>
+                                        <td><?php echo $user->id; ?></td>
+                                        <td><img class="user-image" src="<?php echo $user->placeholderOrImage();?>" alt=""></td>
+                                        <td><?php echo $user->username; ?>
+                                            <div class="action-links">
+                                                <a href="includes/delete-user.php?delete-id=<?php echo $user->id; ?>">Delete</a>
+                                                <a href="edit-users.php?edit-id=<?php echo $user->id; ?>">Edit</a>
+                                                <a href="">View</a>
+                                            </div></td>
+                                        <td><?php echo $user->user_fname; ?></td>
+                                        <td><?php echo $user->user_lname; ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
             <!-- /.row -->
