8809f44663153f35cb4ae082bae79e9d2bb4c696
Gene7oh <gene7oh@hotmail.com>
2023-02-16 14:58:54 -0600

    Create style.css for project root front end modify css in the admin section. add links to init and css in the root index. add code to display images on the home page. work on bootstrap framework to get images to display better

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
-    include("includes/header.php"); ?>
+    include("includes/header.php");
+    $photos = Photo::findAll();
+?>
 <div class="row">
     <!-- Blog Entries Column -->
-    <div class="col-md-8">
+    <div class="col-md-12">
         <h1 class="lead">8 Grid Column.</h1>
+        <div class=" row">
+            <?php foreach ($photos as $photo) : ?>
+                <div class="col-xs-8 col-md-3">
+                    <a class="" href="#">
+                        <img class="img-responsive image-front-end" src="<?php echo 'admin/' . $photo->picturePath(); ?>" alt="image of <?php echo $photo->title; ?>">
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
     <!-- Blog Sidebar Widgets Column -->
-    <div class="col-md-4">
-        <?php
-            include("includes/sidebar.php"); ?>
-    </div>
+    <!--    <div class="col-md-4">
+            
+             <?php //  include("includes/sidebar.php"); ?>
+    </div>-->
     <!-- /.row -->
-    <?php
-        include("includes/footer.php"); ?>
+</div>
+<?php
+    include("includes/footer.php"); ?>
     
 
