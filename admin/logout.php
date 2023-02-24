5ecfda08b48b7c03784de751677647b2fe8b1b8d
Gene7oh <gene7oh@hotmail.com>
2023-02-24 14:58:51 -0600

    Changes to logout php to resetcount();

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,7 @@
 <?php require_once "includes/admin-header.php"; ?>
 <?php
     /** @noinspection PhpUndefinedVariableInspection */
+    $session->resetCount();
     $session->logout();
     redirect("login.php?logout_success");
  ?>
